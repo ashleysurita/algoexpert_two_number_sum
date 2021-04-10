ad2085603a8a97a2e9f811978791f73a38b99edf
Ashley <ashleywong678@gmail.com>
2021-04-09 23:25:38 -0400

    Create my_solution.js

---
+++ my_solution.js
@@ -0,0 +1,17 @@
+function twoNumberSum(array, targetSum) {
+	let sum = []
+  // loop through the array
+	console.log('target', targetSum)
+	array.forEach((num, i) => {
+	// loop through it again, as long as the index is not current index, see if sum is target
+		array.forEach((num2, index) => {
+	// if sum is target, break
+			if((i !== index) && (num + num2 === targetSum)) return sum = [num, num2] 			
+		})
+	})
+	// if no target sum, return empty array
+	return sum
+}
+
+// Do not edit the line below.
+exports.twoNumberSum = twoNumberSum;
